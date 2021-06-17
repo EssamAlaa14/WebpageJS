462e52ca489313409bf857421f116e3c410b3f4f
EssamAlaa14 <essamalaaeldeen22@gmail.com>
2021-06-17 15:27:40 +0200

    updated

--- scripts/contactScript.js
+++ scripts/contactScript.js
@@ -1,122 +1,4 @@
 /* CONTACT US PAGE ---------------------------------------------------------- CONTACT US PAGE */
-// contact us page div container.
-const contactUsDiv = document.querySelector("#contact-content");
-
-/* Creating Form */
-const form = document.createElement("form");
-createFormElements(); //calling form elements.
-function createFormElements() {
-  /* Creating header */
-  const h2El = document.createElement("h2");
-  h2El.innerText = "Leave us a message";
-  h2El.style.marginBottom = "20px";
-  contactUsDiv.appendChild(h2El);
-
-  //Giving Horizontal Row After Heading
-  const line = document.createElement("hr");
-  line.style.marginBottom = "20px";
-  contactUsDiv.appendChild(line);
-
-  /* Adding paragraph */
-  const pEl = document.createElement("p");
-  pEl.innerText = ` Driven by smart technology, everything we do is designed to put the power directly in your hands – giving you the freedom to shop however, whenever and wherever you like.`;
-  pEl.style.marginBottom = "20px";
-  contactUsDiv.appendChild(pEl);
-
-  /* Adding form elements */
-  contactUsDiv.appendChild(form);
-  //Name label
-  const label1 = document.createElement("label");
-  label1.innerText = "Name";
-  form.appendChild(label1);
-  //Email label
-  const label2 = document.createElement("label");
-  label2.innerText = "Email";
-  label2.style.marginLeft = "580px";
-  form.appendChild(label2);
-  //Line break
-  const lineBreak1 = document.createElement("br");
-  form.appendChild(lineBreak1);
-
-  //Name field
-  const name = document.createElement("input");
-  name.setAttribute("id", "name");
-  name.setAttribute("type", "text");
-  name.setAttribute("name", "Name");
-  name.setAttribute("size", "56px");
-  name.setAttribute("required", "true");
-  form.appendChild(name);
-
-  //Email field
-  const email = document.createElement("input");
-  email.setAttribute("id", "email");
-  email.setAttribute("type", "email");
-  email.setAttribute("name", "Email");
-  email.setAttribute("size", "56px");
-  email.setAttribute("required", "true");
-  email.style.marginLeft = "130px";
-  form.appendChild(email);
-
-  //Line breaks
-  const lineBreak2 = document.createElement("br");
-  form.appendChild(lineBreak2);
-  const lineBreak3 = document.createElement("br");
-  form.appendChild(lineBreak3);
-
-  //Subject label
-  const label3 = document.createElement("label");
-  label3.innerHTML = "Subject";
-  form.appendChild(label3);
-  //Line break
-  const lineBreak4 = document.createElement("br");
-  form.appendChild(lineBreak4);
-
-  //Subject field
-  const subject = document.createElement("input");
-  subject.setAttribute("id", "subject");
-  subject.setAttribute("type", "text");
-  subject.setAttribute("name", "Subject");
-  subject.setAttribute("size", "133px");
-  subject.setAttribute("required", "true");
-  form.appendChild(subject);
-
-  //Line breaks
-  const lineBreak5 = document.createElement("br");
-  form.appendChild(lineBreak5);
-  const lineBreak6 = document.createElement("br");
-  form.appendChild(lineBreak6);
-
-  //Message label
-  const label4 = document.createElement("label");
-  label4.innerHTML = "Message";
-  form.appendChild(label4);
-  //Line break
-  const lineBreak7 = document.createElement("br");
-  form.appendChild(lineBreak7);
-
-  //Message field
-  const message = document.createElement("textarea");
-  message.setAttribute("id", "txtMsg");
-  message.setAttribute("name", "Message");
-  message.setAttribute("cols", "135px");
-  message.setAttribute("rows", "10px");
-  message.setAttribute("required", "true");
-  form.appendChild(message);
-
-  //Line breaks
-  const lineBreak8 = document.createElement("br");
-  form.appendChild(lineBreak8);
-  const lineBreak9 = document.createElement("br");
-  form.appendChild(lineBreak9);
-
-  //Submit Button
-  const btn = document.createElement("button");
-  btn.innerText = "Send message";
-  btn.setAttribute("type", "submit");
-  btn.setAttribute("value", "submit");
-  btn.setAttribute("name", "Submit");
-  form.appendChild(btn);
-}
 
 /* POSTING Form Data */
 //Contact us link
@@ -124,7 +6,8 @@ const contactUsAPILink =
   "https://afternoon-falls-30227.herokuapp.com/api/v1/contact_us";
 
 // add submit Event listener
-form.addEventListener("submit", (event) => {
+let formSubmitBtn = document.querySelector("#submitBtn");
+formSubmitBtn.addEventListener("click", (event) => {
   // prevent default
   event.preventDefault();
   //Input fields...
